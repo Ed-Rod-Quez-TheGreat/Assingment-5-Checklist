cc47214131d7ae92db0ad1f13a10501a2cee9b83
Edward Rodriquez <Ed.R.Rodriquez@gmail.com>
2020-07-24 21:49:02 -0600

    Added raw JSON data, input destination into test template

--- script.js
+++ script.js
@@ -3,11 +3,11 @@
 /* This block of code shows how to format the HTML once you fetch some planetary JSON!
 <h2>Mission Destination</h2>
 <ol>
-   <li>Name: ${}</li>
-   <li>Diameter: ${}</li>
-   <li>Star: ${}</li>
-   <li>Distance from Earth: ${}</li>
-   <li>Number of Moons: ${}</li>
+   <li>Name: ${"Jupiter/Europa"}</li>
+   <li>Diameter: ${"3,121.6 km"}</li>
+   <li>Star: ${"Sol"}</li>
+   <li>Distance from Earth: ${"628.3 million km from Earth"}</li>
+   <li>Number of Moons: ${0}</li>
 </ol>
-<img src="${}">
+<img src="${"https://apod.nasa.gov/apod/image/1609/Europa_Galileo_960.jpg"}">
 */
