fa86d43a0e6d0db1b485ee34765b37401c4dafd0
Edward Rodriquez <Ed.R.Rodriquez@gmail.com>
2020-07-26 22:25:13 -0600

    Removed json reference txt, added blank field popup alert. Confirmed functional w/any missing field

--- script.js
+++ script.js
@@ -17,3 +17,37 @@ window.addEventListener("load", function () {
       })
    });
 });
+
+/*let form = document.querySelector("form");
+let launchStatus = document.getElementById("launchStatus");
+let faultyItems = document.getElementById("faultyItems");
+
+form.addEventListener("submit", function (event) {
+   let pilotName = document.querySelector("input[name=pilotName]");
+   let copilotName = document.querySelector("input[name=copilotName]");
+   let fuelLevel = document.querySelector("input[name=fuelLevel]");
+   let cargoMass = document.querySelector("input[name=cargoMass]");
+
+   if (pilotName.value === "" || copilotName.value === "" ||
+      fuelLevel.value === "" || cargoMass.value === "") {
+      alert("All fields are required!");
+      event.preventDefault();
+   };
+});*/
+window.addEventListener("load", function() {
+   let form = document.querySelector("form");
+   let launchStatus = document.getElementById("launchStatus");
+   let faultyItems = document.getElementById("faultyItems");
+   form.addEventListener("submit", function (event) {
+      let pilotName = document.querySelector("input[name=pilotName]");
+      let copilotName = document.querySelector("input[name=copilotName]");
+      let fuelLevel = document.querySelector("input[name=fuelLevel]");
+      let cargoMass = document.querySelector("input[name=cargoMass]");
+
+      if (pilotName.value === "" || copilotName.value === "" ||
+         fuelLevel.value === "" || cargoMass.value === "") {
+         alert("All fields are required!");
+         event.preventDefault();
+      };
+   });
+});
